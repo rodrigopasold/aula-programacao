e7dbc9d340d092cfaca5c57a8c47e7ff1bfd4804
gelvazio <gelvazio@tidas.com.br>
2022-12-27 10:56:57 -0300

    ok

---
+++ contentDetails.js
@@ -0,0 +1,251 @@
+console.clear()
+
+let id = location.search.split('?')[1];
+
+console.log(id);
+
+if(document.cookie.indexOf(',counter=')>=0)
+{
+    let counter = document.cookie.split(',')[1].split('=')[1]
+    document.getElementById("badge").innerHTML = counter
+}
+
+function dynamicContentDetails(ob)
+{
+    let mainContainer = document.createElement('div')
+    mainContainer.id = 'containerD'
+    document.getElementById('containerProduct').appendChild(mainContainer);
+
+    let imageSectionDiv = document.createElement('div')
+    imageSectionDiv.id = 'imageSection'
+
+    let imgTag = document.createElement('img')
+     imgTag.id = 'imgDetails'
+     //imgTag.id = ob.photos
+     imgTag.src = ob.preview
+
+    imageSectionDiv.appendChild(imgTag)
+
+    let productDetailsDiv = document.createElement('div')
+    productDetailsDiv.id = 'productDetails'
+
+    // console.log(productDetailsDiv);
+
+    let h1 = document.createElement('h1')
+    let h1Text = document.createTextNode(ob.name)
+    h1.appendChild(h1Text)
+
+    let h4 = document.createElement('h4')
+    let h4Text = document.createTextNode(ob.brand)
+    h4.appendChild(h4Text)
+    console.log(h4);
+
+    let detailsDiv = document.createElement('div')
+    detailsDiv.id = 'details'
+
+    let h3DetailsDiv = document.createElement('h3')
+    let h3DetailsText = document.createTextNode('R$ ' + ob.price)
+    h3DetailsDiv.appendChild(h3DetailsText)
+
+    let h3 = document.createElement('h3')
+    let h3Text = document.createTextNode('Description')
+    h3.appendChild(h3Text)
+
+    let para = document.createElement('p')
+    let paraText = document.createTextNode(ob.description)
+    para.appendChild(paraText)
+
+    let productPreviewDiv = document.createElement('div')
+    productPreviewDiv.id = 'productPreview'
+
+    let h3ProductPreviewDiv = document.createElement('h3')
+    let h3ProductPreviewText = document.createTextNode('Product Preview')
+    h3ProductPreviewDiv.appendChild(h3ProductPreviewText)
+    productPreviewDiv.appendChild(h3ProductPreviewDiv)
+
+    let i;
+    for(i=0; i<ob.photos.length; i++)
+    {
+        let imgTagProductPreviewDiv = document.createElement('img')
+        imgTagProductPreviewDiv.id = 'previewImg'
+        imgTagProductPreviewDiv.src = ob.photos[i]
+        imgTagProductPreviewDiv.onclick = function(event)
+        {
+            console.log("clicked" + this.src)
+            imgTag.src = ob.photos[i]
+            document.getElementById("imgDetails").src = this.src 
+            
+        }
+        productPreviewDiv.appendChild(imgTagProductPreviewDiv)
+    }
+
+    let buttonDiv = document.createElement('div')
+    buttonDiv.id = 'button'
+
+    let buttonTag = document.createElement('button')
+    buttonDiv.appendChild(buttonTag)
+
+    buttonText = document.createTextNode('Add to Cart')
+    buttonTag.onclick  =   function()
+    {
+        let order = id+" "
+        let counter = 1
+        if(document.cookie.indexOf(',counter=')>=0)
+        {
+            order = id + " " + document.cookie.split(',')[0].split('=')[1]
+            counter = Number(document.cookie.split(',')[1].split('=')[1]) + 1
+        }
+        document.cookie = "orderId=" + order + ",counter=" + counter
+        document.getElementById("badge").innerHTML = counter
+        console.log(document.cookie)
+    }
+    buttonTag.appendChild(buttonText)
+
+
+    console.log(mainContainer.appendChild(imageSectionDiv));
+    mainContainer.appendChild(imageSectionDiv)
+    mainContainer.appendChild(productDetailsDiv)
+    productDetailsDiv.appendChild(h1)
+    productDetailsDiv.appendChild(h4)
+    productDetailsDiv.appendChild(detailsDiv)
+    detailsDiv.appendChild(h3DetailsDiv)
+    detailsDiv.appendChild(h3)
+    detailsDiv.appendChild(para)
+    productDetailsDiv.appendChild(productPreviewDiv)
+    
+    
+    productDetailsDiv.appendChild(buttonDiv)
+
+
+    return mainContainer
+}
+
+function callApiDetail(){
+    
+    let httpRequest = new XMLHttpRequest()
+    {
+        httpRequest.onreadystatechange = function()
+        {
+            if(this.readyState === 4 && this.status == 200)
+            {
+                console.log('connected!!');
+
+                let contentDetails = JSON.parse(this.responseText)
+                {
+                    console.log(contentDetails);
+
+                    dynamicContentDetails(contentDetails)
+                }
+            }
+            else
+            {
+                console.log('not connected!');
+            }
+        }
+    }
+
+    httpRequest.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product/' + id, true)
+    httpRequest.send();
+}
+
+function getDadosLocais(id){
+    var dadosRetorno = 
+                `
+            {
+        "id": "999999",
+        "name": "Produto nao editado!",
+        "preview": "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+        "photos": [
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg"
+        ],
+        "description": "Produto nao editado!",
+        "size": [
+            1,
+            1,
+            0,
+            1,
+            0
+        ],
+        "isAccessory": false,
+        "brand": "Produto nao editado!",
+        "price": 99999
+        }`;
+
+    if(id == 1){
+        dadosRetorno = 
+                `
+            {
+        "id": "1",
+        "name": "Men Navy Blue Solid Sweatshirt",
+        "preview": "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+        "photos": [
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg"
+        ],
+        "description": "Navy solid sweatshirt with patchwork, has a round neck, long sleeves, straight hem",
+        "size": [
+            1,
+            1,
+            0,
+            1,
+            0
+        ],
+        "isAccessory": false,
+        "brand": "United Colors of Benetton - Josiane camargo",
+        "price": 2599
+        }`;
+    }
+    
+    if(id == 2){
+        dadosRetorno = 
+                `
+            {
+        "id": "2",
+        "name": "Ola rodrigo...",
+        "preview": "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+        "photos": [
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg",
+            "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg"
+        ],
+        "description": "ola mundo",
+        "size": [
+            1,
+            1,
+            0,
+            1,
+            0
+        ],
+        "isAccessory": false,
+        "brand": "Produto de codigo 2",
+        "price": 2599
+        }`;
+    }
+    
+    return dadosRetorno;
+    
+}
+
+function callApiDadosLocais(id){
+    //var id = 1;
+    let contentDetails = getDadosLocais(id);
+
+    contentDetails = JSON.parse(contentDetails);
+
+    dynamicContentDetails(contentDetails);
+}
+
+
+// BACKEND CALLING
+// callApiDetail();
+
+callApiDadosLocais(id);
